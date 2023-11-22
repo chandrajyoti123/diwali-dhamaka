d614d3a73e2f1b23d539b5bd653ffd31a2737ecf
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-11-23 01:41:23 +0530

    greeting card made

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import stars from './img/stars.png'
+import rose from './img/rose.png'
+
+import './Home.css'
 import { useSearchParams } from 'react-router-dom'
- const greeting=['happy diwali','happy dussehara','happy holi']
+ const greeting=['A New Year is like a blank book, and the pen is in your hands. It is your chance to write a beautiful story for yourself. Happy New Year.','Every end marks a new beginning. Keep your spirits and determination unshaken, and you shall always walk the glory road. With courage, faith and great effort, you shall achieve everything you desire. I wish you a Happy New Year.','You’re supposed to let go of the past and start off new. You’re supposed to forgive all those who have hurt you and be open to new relationships, with open arms. That is why it is called the ‘New’ Year. May you have a Happy New Year','We will open the book. Its pages are blank. We are going to put words on them ourselves. The book is called Opportunity, and its first chapter is New Year’s Day','At precisely the moment you feel like giving up, victory is always within reach. Remember this moving into the New Year.']
+ const theme=['theme-1','theme-2','theme-3','theme-4']
 export default function Home() {
   const [searchParams]=useSearchParams();
-  const to=searchParams.get("to")
-  const from=searchParams.get("from")
+  const [to,setTo]=useState(searchParams.get("to"))
+  const [from ,setFrom]=useState(searchParams.get("from"))
+  
+  
+ 
   const g=searchParams.get("g")>greeting.length?0:searchParams.get("g")||0
+  const t=searchParams.get('t')
   return (
     <div>
-      <h1>this is home page</h1>
-      <div>dear {to} this greeting from {from} and I wish you {greeting[g]} </div>
+    <div className={`main-container ${theme[t]}`}>
+     
+      <div className='greet-body'>
+      <h1 className='heading'><img   src={stars} className='stars'/>Happy New Year<img  src={stars} className='stars'/></h1>
+      <div className='greet-to'>Dear {to}</div>
+      <div className='greet-message'>{greeting[g]}</div>
+      <div className='greet-form'> <img src={rose} className='rose'/>Regards {from}</div>
+      </div>
+    
+   </div>
+   <div className='text-to-customize'>Do you want to create your own Diwali Greeting? Customize it here 👇</div>
+   <input type='text' placeholder='name of your friend' onChange={(e)=>{
 
-    </div>
+   }} />
+   </div>
+  
+
   )
 }
